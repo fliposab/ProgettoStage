4d0ba7c5a74df36f0d36f21e5e0b45ce27f8123c
fliposab <filipposabbadin.2001@gmail.com>
2025-07-29 11:08:22 +0200

    change

--- Sviluppo/Godot/gut.sh
+++ Sviluppo/Godot/gut.sh
@@ -35,6 +35,4 @@ curl -s -S ${GODOT_URL} --output ${GODOT_ZIP}
 unzip -q ${GODOT_ZIP}
 
 chmod +x ${GODOT_BIN}
-./${GODOT_BIN} -d -s --path $GITHUB_WORKSPACE addons/gut/gut_cmdln.gd -gdir=res://test -ginclude_subdirs -gexit
-
-rm -f ${GODOT_BIN} ${GODOT_ZIP}
+./${GODOT_BIN} -d -s --path $GITHUB_WORKSPACE addons/gut/gut_cmdln.gd -gdir=res://test -ginclude_subdirs -gexitgit
